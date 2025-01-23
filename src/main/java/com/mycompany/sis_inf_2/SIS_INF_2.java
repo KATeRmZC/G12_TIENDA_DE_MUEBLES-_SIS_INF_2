0aeb69b673eb775aefa6dd06c91920c0f20bf741
CORE i5 <CORE i5@192.168.0.101>
2025-01-23 14:39:36 -0400

    cARGA INICIAL

---
+++ src/main/java/com/mycompany/sis_inf_2/SIS_INF_2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.sis_inf_2;
+
+/**
+ *
+ * @author CORE i5
+ */
+public class SIS_INF_2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
